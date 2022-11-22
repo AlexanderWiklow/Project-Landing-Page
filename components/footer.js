8f944c6ccfb63120e67db14683fcd6e3c2d4978f
AlexanderWiklow <a.wiklow@gmail.com>
2022-11-22 12:01:08 +0100

    initial commit

---
+++ components/footer.js
@@ -0,0 +1,44 @@
+const footerTemplate = document.createElement('template')
+
+footerTemplate.innerHTML = `
+        <link rel="stylesheet" href="../stylesheets/styles.css"><footer id="footer">
+        <div class="footer-container">
+            <div class="social-media-icons">
+
+                <a href="https://www.facebook.com/" target="_blank">
+                    <img class="social-media-icons-footer" src="./assets/images/icons/icon1.png" alt="">
+                </a>
+
+                <a href="https://www.instagram.com/" target="_blank">
+                    <img class="social-media-icons-footer" src="./assets/images/icons/icon2.png" alt="">
+                </a>
+
+                <a class="social-media-links" href="https://www.youtube.com/" target="_blank">
+                    <img class="social-media-icons-footer" src="./assets/images/icons/icon3.png" alt="">
+                </a>
+            </div>
+
+            <div class="footer-links">
+                <a class="footer-links" id="name" href="./index.html" class="active">HIKING&STUFF</a>
+                <a class="footer-links" href="./product-list-page.html">Products</a>
+                <a class="footer-links" href="./add-product-form.html">Add product</a>
+                <a class="footer-links" href="./about-us.html">About us</a>
+            </div>
+        </div>
+    </footer>`
+
+class Footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const shadowRoot = this.attachShadow({
+            mode: 'closed'
+        });
+
+        shadowRoot.appendChild(footerTemplate.content);
+    }
+}
+
+customElements.define('footer-component', Footer)
---
+++ components/header.js
@@ -0,0 +1,29 @@
+const headerTemplate = document.createElement('template')
+
+headerTemplate.innerHTML = `    <link rel="stylesheet" href="./stylesheets/styles.css"><nav id="main-nav">
+            <a id="name" href="./index.html" class="active">HIKING&STUFF</a>
+            <a href="./product-list-page.html">Products</a>
+            <a href="./add-product-form.html">Add product</a>
+            <a href="./about-us.html">About us</a>
+
+            <!-- TODO: dark mode -->
+            <!-- <button id="dark-mode-btn" onclick="myFunction3()">Dark Mode</button> -->
+        </nav>`
+
+
+class Header extends HTMLElement {
+
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const shadowRoot = this.attachShadow({
+            mode: 'closed'
+        });
+
+        shadowRoot.appendChild(headerTemplate.content);
+    }
+}
+
+customElements.define('header-component', Header)
