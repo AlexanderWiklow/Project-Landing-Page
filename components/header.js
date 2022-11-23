4e20856bd0ac481103a75216c101fad0da97dffc
AlexanderWiklow <a.wiklow@gmail.com>
2022-11-23 23:08:20 +0100

    change folder structure

--- components/header.js
+++ components/header.js
@@ -1,10 +1,10 @@
 const headerTemplate = document.createElement('template')
 
 headerTemplate.innerHTML = `    <link rel="stylesheet" href="../stylesheets/styles.css"><nav id="main-nav">
-            <a id="name" href="../docs/index.html" class="active">HIKING&STUFF</a>
-            <a href="../docs/product-list-page.html">Products</a>
-            <a href="../docs/add-product-form.html">Add product</a>
-            <a href="../docs/about-us.html">About us</a>
+            <a id="name" href="../index.html" class="active">HIKING&STUFF</a>
+            <a href="../product-list-page.html">Products</a>
+            <a href="../add-product-form.html">Add product</a>
+            <a href="../about-us.html">About us</a>
 
             <!-- TODO: dark mode -->
             <!-- <button id="dark-mode-btn" onclick="myFunction3()">Dark Mode</button> -->
