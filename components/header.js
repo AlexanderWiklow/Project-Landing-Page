e255e4adbf2a9a03d55a0425ba9d22d94205e18e
AlexanderWiklow <a.wiklow@gmail.com>
2022-11-24 17:35:53 +0100

    give font 6vw

--- components/header.js
+++ components/header.js
@@ -1,6 +1,6 @@
 const headerTemplate = document.createElement('template')
 
-headerTemplate.innerHTML = `<head><link rel="stylesheet" href="stylesheets/styles.css"></head><nav id="main-nav">
+headerTemplate.innerHTML = `<link rel="stylesheet" href="stylesheets/styles.css"><nav id="main-nav">
             <a id="name" href="./index.html" class="active">HIKING&STUFF</a>
             <a href="./product-list-page.html">Products</a>
             <a href="./add-product-form.html">Add product</a>
