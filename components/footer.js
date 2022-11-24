b50aa8d62373611b0b5c1d9fe1a55a5467638406
AlexanderWiklow <a.wiklow@gmail.com>
2022-11-24 10:44:43 +0100

    change paths

--- components/footer.js
+++ components/footer.js
@@ -1,28 +1,28 @@
 const footerTemplate = document.createElement('template')
 
 footerTemplate.innerHTML = `
-        <link rel="stylesheet" href="../stylesheets/styles.css"><footer id="footer">
+        <link rel="stylesheet" href="stylesheets/styles.css"><footer id="footer">
         <div class="footer-container">
             <div class="social-media-icons">
 
                 <a href="https://www.facebook.com/" target="_blank">
-                    <img class="social-media-icons-footer" src="../assets/images/icons/icon1.png" alt="">
+                    <img class="social-media-icons-footer" src="./assets/images/icons/icon1.png" alt="">
                 </a>
 
                 <a href="https://www.instagram.com/" target="_blank">
-                    <img class="social-media-icons-footer" src="../assets/images/icons/icon2.png" alt="">
+                    <img class="social-media-icons-footer" src="./assets/images/icons/icon2.png" alt="">
                 </a>
 
                 <a class="social-media-links" href="https://www.youtube.com/" target="_blank">
-                    <img class="social-media-icons-footer" src="../assets/images/icons/icon3.png" alt="">
+                    <img class="social-media-icons-footer" src="./assets/images/icons/icon3.png" alt="">
                 </a>
             </div>
 
             <div class="footer-links">
                 <a class="footer-links" id="name" href="./index.html" class="active">HIKING&STUFF</a>
-                <a class="footer-links" href="../product-list-page.html">Products</a>
-                <a class="footer-links" href="../add-product-form.html">Add product</a>
-                <a class="footer-links" href="../about-us.html">About us</a>
+                <a class="footer-links" href="./product-list-page.html">Products</a>
+                <a class="footer-links" href="./add-product-form.html">Add product</a>
+                <a class="footer-links" href="./about-us.html">About us</a>
             </div>
         </div>
     </footer>`
