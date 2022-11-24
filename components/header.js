dc89004df2126efb49594bad3dbf5a9e7fa8bfa5
AlexanderWiklow <a.wiklow@gmail.com>
2022-11-24 18:56:40 +0100

    font variables

--- components/header.js
+++ components/header.js
@@ -5,18 +5,18 @@ headerTemplate.innerHTML = `<link rel="stylesheet" href="stylesheets/styles.css"
             <a href="./product-list-page.html">Products</a>
             <a href="./add-product-form.html">Add product</a>
             <a href="./about-us.html">About us</a>
-
-            <!-- TODO: dark mode -->
-            <!-- <button id="dark-mode-btn" onclick="myFunction3()">Dark Mode</button> -->
         </nav>`
 
-
+// Skapar en klass 'Header' som ärver egenskaper från HTMLElement. Det blir en child class.
 class Header extends HTMLElement {
 
+    //
     constructor() {
+        // Super() metoden refererar till parent och får samma metoder och får även tillgång till samma properties. 
         super();
     }
 
+    // ShadowRoot tillåter att ett dolt DOM-struktur kan användas på den synliga DOM-delen. 
     connectedCallback() {
         const shadowRoot = this.attachShadow({
             mode: 'closed'
